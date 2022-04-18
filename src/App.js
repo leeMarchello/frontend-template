c3646f2279f7eac425d04770c36df02f212a8656
Lee Opperman <lee.opperman5@gmail.com>
2022-04-19 00:28:01 +0200

    Create react app entry point

---
+++ src/App.js
@@ -0,0 +1,8 @@
+import React from 'react';
+export const App = () => {
+  return (
+    <div container="App">
+      <h1>This is the react App </h1>
+    </div>
+  );
+};
--- src/test.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import { render } from 'react-dom';
-render(
-  React.createElement('h1', {}, 'webpack is configured, React is loaded!!'),
-  document.getElementById('root'),
-);
